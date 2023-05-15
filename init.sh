3bcb12a94a2602cdc5da4ab0db4575d5dfaac503
Eddy Ding <edwarding04@gmail.com>
2023-05-15 15:45:05 -0400

    Add more documentation

--- init.sh
+++ init.sh
@@ -21,25 +21,25 @@ then
 fi
 pages=$(pdfinfo ${filename}  | awk "/^Pages:/ {print $4}" | sed s/"Pages: "//)  #CREATE VARIABLE WITH NUMBER OF PAGES IN FILE
 
-cd .. 
+cd ..                                     #STEPPING BACK TO MAIN DIRECTORY
 
-for i in $(seq $pages); 
+for i in $(seq $pages);                                                     #LOOP WHICH ADDS target.pdf AFTER EVERY PAGE
 do
-    pdftk ./input/$filename cat $i output "$i.pdf"
+    pdftk ./input/$filename cat $i output "$i.pdf"                          #$i.pdf IS THE TEMPORARY FILE FOR THE INDIVIDUAL PAGE
     if [[ $i -eq 1 ]];
     then
-        pdftk "$i.pdf" ./target/"target.pdf" cat output "latest.pdf"
+        pdftk "$i.pdf" ./target/"target.pdf" cat output "latest.pdf"        #1st Iteration: Combine the initial page with target into latest.pdf
     else
-        mv latest.pdf previous.pdf
+        mv latest.pdf previous.pdf                                          #Additional Iterations: latest.pdf gets turned into previous, the new page (i.pdf) is added into the new latest.pdf
         pdftk "previous.pdf" "$i.pdf" cat output "latest.pdf"
-        rm previous.pdf
+        rm previous.pdf                                                     #previous.pdf is deleted and the new latest is now previous
         mv latest.pdf previous.pdf
-        pdftk "previous.pdf" ./target/"target.pdf" cat output "latest.pdf"
+        pdftk "previous.pdf" ./target/"target.pdf" cat output "latest.pdf"  #target.pdf is appended and previous.pdf is deleted
         rm previous.pdf
     fi
     rm $i.pdf
 done
 
-newname=$(echo $filename | sed s/".pdf"//)
+newname=$(echo $filename | sed s/".pdf"//)                                  #STORE NEW NAME FOR OUPUT FILE (original name + xtra.pdf)
 mv latest.pdf "$newname-xtra.pdf"
-mv "$newname-xtra.pdf" ./output
+mv "$newname-xtra.pdf" ./output                                             #RENAME FILE AND PUT INTO /ouput
