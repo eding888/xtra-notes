228b467379cc9dad5ad001e36a1e8825b9d99d0a
Eddy Ding <edwarding04@gmail.com>
2023-05-15 15:01:21 -0400

    Add comments

--- init.sh
+++ init.sh
@@ -1,9 +1,9 @@
-current_dir=$(dirname "$0")
-input_dir=${current_dir}/input
-filename=$(basename ${input_dir}/*.pdf)
+current_dir=$(dirname "$0")               #CURRENT DIRECTORY
+input_dir=${current_dir}/input            #STORE DIRECTORY OF /input FOLDER
+filename=$(basename ${input_dir}/*.pdf)   #GET THE FILENAME OF PDF IN /input FOLDER
 
 
-if [[ $filename = *" "* ]];
+if [[ $filename = *" "* ]];               #CHECK TO SEE IF THERE IS A SPACE IN THE INPUT FILENAME
 then
     echo "---------------------------"
     echo "No spaces in input filname!"
@@ -11,17 +11,16 @@ then
     exit 0
 fi
 
-cd input
+cd input                                  #ENTERING INPUT DIRECTORY FOR FUTURE COMMANDS
 
-if (( $(ls -l | wc -l) > 2 ));
+if (( $(ls -l | wc -l) > 2 ));            #CHECK TO SEE IF THERE IS ONLY 1 FILE IN /input FOLDER
 then
     echo "-----------------------------------------------------"
     echo "xtra-notes currently only supports 1 file at a time."
     echo "-----------------------------------------------------"
     exit 0
 fi
-pages=$(pdfinfo ${filename}  | awk "/^Pages:/ {print $4}" | sed s/"Pages: "//)
+pages=$(pdfinfo ${filename}  | awk "/^Pages:/ {print $4}" | sed s/"Pages: "//)  #CREATE VARIABLE WITH NUMBER OF PAGES IN FILE
 for i in pages 
 do
-    echo $pages
 done
