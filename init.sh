0fea5a6a3b5ff87939b60ea531d437a42e0a38a6
Eddy Ding <edwarding04@gmail.com>
2023-05-15 15:55:31 -0400

    Add messages when running script

--- init.sh
+++ init.sh
@@ -1,26 +1,36 @@
+red=$'\e[1;31m'
+green=$'\e[1;32m'
+blue=$'\e[1;34m'
+
+
+
+
 current_dir=$(dirname "$0")               #CURRENT DIRECTORY
 input_dir=${current_dir}/input            #STORE DIRECTORY OF /input FOLDER
 filename=$(basename ${input_dir}/*.pdf)   #GET THE FILENAME OF PDF IN /input FOLDER
 
+    echo "$blue Filename acquired..."
+
 if [[ $filename = *" "* ]];               #CHECK TO SEE IF THERE IS A SPACE IN THE INPUT FILENAME
 then
-    echo "---------------------------"
-    echo "No spaces in input filname!"
-    echo "---------------------------"
+    echo "$red ---------------------------"
+    echo "$red No spaces in input filname!"
+    echo "$red ---------------------------"
     exit 0
 fi
+
 
 cd input                                  #ENTERING INPUT DIRECTORY FOR FUTURE COMMANDS
 
 if (( $(ls -l | wc -l) > 2 ));            #CHECK TO SEE IF THERE IS ONLY 1 FILE IN /input FOLDER
 then
-    echo "-----------------------------------------------------"
-    echo "xtra-notes currently only supports 1 file at a time."
-    echo "-----------------------------------------------------"
+    echo "$red -----------------------------------------------------"
+    echo "$red xtra-notes currently only supports 1 file at a time."
+    echo "$red -----------------------------------------------------"
     exit 0
 fi
 pages=$(pdfinfo ${filename}  | awk "/^Pages:/ {print $4}" | sed s/"Pages: "//)  #CREATE VARIABLE WITH NUMBER OF PAGES IN FILE
-
+    echo "$green Initalizing..."
 cd ..                                     #STEPPING BACK TO MAIN DIRECTORY
 
 for i in $(seq $pages);                                                     #LOOP WHICH ADDS target.pdf AFTER EVERY PAGE
@@ -38,8 +48,12 @@ do
         rm previous.pdf
     fi
     rm $i.pdf
+    echo "$blue Page $i complete..."
 done
 
 newname=$(echo $filename | sed s/".pdf"//)                                  #STORE NEW NAME FOR OUPUT FILE (original name + xtra.pdf)
 mv latest.pdf "$newname-xtra.pdf"
 mv "$newname-xtra.pdf" ./output                                             #RENAME FILE AND PUT INTO /ouput
+
+    echo "$green All done :). Please check ./output for your finished file!"
+    echo $'\e[1;0m'
