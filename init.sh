ee912db1b6f9c12d2d6699035813563c98d2a90d
Eddy Ding <edwarding04@gmail.com>
2023-05-15 14:57:53 -0400

    Add check for > 1 input file

--- init.sh
+++ init.sh
@@ -1,6 +1,8 @@
 current_dir=$(dirname "$0")
 input_dir=${current_dir}/input
 filename=$(basename ${input_dir}/*.pdf)
+
+
 if [[ $filename = *" "* ]];
 then
     echo "---------------------------"
@@ -10,6 +12,14 @@ then
 fi
 
 cd input
+
+if (( $(ls -l | wc -l) > 2 ));
+then
+    echo "-----------------------------------------------------"
+    echo "xtra-notes currently only supports 1 file at a time."
+    echo "-----------------------------------------------------"
+    exit 0
+fi
 pages=$(pdfinfo ${filename}  | awk "/^Pages:/ {print $4}" | sed s/"Pages: "//)
 for i in pages 
 do
